4bca82b80d930aa7a5a6f2193b9f4f14b8f9759a
Gabriele Viveiros De Brito <38940021827@info.scs>
2022-02-25 10:19:00 -0300

    login

--- Projeto Ofertas/projeto-ofertas/src/pages/login.jsx
+++ Projeto Ofertas/projeto-ofertas/src/pages/login.jsx
@@ -19,49 +19,6 @@ export default class Login extends Component {
         };
     }
 
-    // efetuarLogin = (evento) => {
-    //     evento.preventDefault();
-
-    //     this.setState({ mensagemErro: '', isLoading: true })
-    //     axios.post('http://192.168.3.115:5000/api/Login', {
-    //         email: this.state.email,
-    //         senha: this.state.senha
-    //     })
-    //         .then(resposta => {
-    //             if (resposta.status === 200) {
-    //                 localStorage.setItem('usuario-login', resposta.data.token);
-    //                 this.setState({ isLoading: false })
-    //                 //console.log(localStorage.getItem('usuario-login'))
-    //                 switch (parsejwt().role) {
-    //                     case '2':
-    //                         this.props.history.push('/consultasmedico')
-    //                         break;
-    //                     case '3':
-    //                         this.props.history.push('/consultaspaciente')
-    //                         break;
-    //                     case '1':
-    //                         this.props.history.push('/todasasconsultas')
-    //                         break;
-
-    //                     default:
-    //                         console.log('Ué cara')
-    //                         break;
-    //                 }
-    //             }
-    //         })
-    //         .catch(() => {
-    //             this.setState({
-    //                 mensagemErro: 'Email ou senha invalido',
-    //                 isLoading: false,
-    //                 email: '',
-    //                 senha: ''
-    //             }, console.log('deu errado'))
-    //         })
-    // }
-
-    // atualizaStateCampo = (campo) => {
-    //     this.setState({ [campo.target.name]: campo.target.value });
-    // };
 
     render() {
         return (
@@ -78,20 +35,23 @@ export default class Login extends Component {
                         </div>
                         </section>
                         <section className="container-box-form">
-                            <div>
-                                <h2>SEJA BEM VINDO.</h2>
+                            <div className="frase">
+                                <h2>SEJA<br/> BEM <br/>VINDO.</h2>
                             </div>
                         <div className="box-form">
-                            <h1>Login</h1>
                             <form className="form" onSubmit={this.efetuarLogin}>
                                 <div className="box-input-login">
-                                <label for="nome_usuario">E-mail</label>  
-                                    <input type="text" placeholder="exemplo@email.com" name="email" value={this.state.email} onChange={this.atualizaStateCampo} />
-                                    <label for="nome_usuario">Senha</label>  
-                                    <input type="password" placeholder="**********" name="senha" value={this.state.senha} onChange={this.atualizaStateCampo} />
+                                    <div className="email">
+                                        <label className="email_usuario" for="nome_usuario">E-mail</label>  
+                                        <input className="email_caixa" type="text" placeholder="exemplo@email.com" name="email" value={this.state.email} onChange={this.atualizaStateCampo} />
+                                    </div>
+                                    <div className="senha">
+                                        <label className="senha_usuario" for="senha_usuario">Senha</label>  
+                                        <input className="senha_caixa" type="password" placeholder="**********" name="senha" value={this.state.senha} onChange={this.atualizaStateCampo} />
+                                    </div>
                                     <span className="Mensagem-erro">{this.state.mensagemErro}</span>
                                     {
-                                        this.state.isLoading === true ? <button disabled>Entrando...</button>: <button className="Entrando">Entrar</button>
+                                        this.state.isLoading === true ? <button disabled>Entrando...</button>: <button className="Entrando">Login</button>
                                     }
                                 </div>
                             </form>
