ca8fc1c37727eaf9f6fb06b8c5d6747743be4745
livianegrini <liviasnegrini2004@gmail.com>
2022-02-28 14:51:36 -0300

    Adicionando DDL

--- BD/Scripts/Ofertas-DDL.sql
+++ BD/Scripts/Ofertas-DDL.sql
@@ -17,6 +17,7 @@ GO
 
 CREATE TABLE Endereco(
    IdEndereco INT PRIMARY KEY IDENTITY(1,1),
+   IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
    Logadouro VARCHAR(70) NOT NULL,
    Numero SMALLINT NOT NULL,
    Bairro VARCHAR(50) NOT NULL,
@@ -26,43 +27,21 @@ CREATE TABLE Endereco(
 );
 GO
 
-CREATE TABLE Empresa(
-   IdEmpresa INT PRIMARY KEY IDENTITY(1,1),
-   IdEndereco INT FOREIGN KEY REFERENCES Endereco(IdEndereco),
+CREATE TABLE Instituicao(
+   IdInstituicao INT PRIMARY KEY IDENTITY(1,1),
    IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
    CNPJ VARCHAR(14) NOT NULL,
-   CNAE VARCHAR(7) NOT NULL,
-   NomeFantasia VARCHAR(70) NOT NULL,
+   NomeFantasia VARCHAR(50) NOT NULL,
    RazaoSocial  VARCHAR(70) NOT NULL
-);
-GO
-
-CREATE TABLE ONG(
-	IdONG INT PRIMARY KEY IDENTITY(1,1),
-	IdEndereco INT FOREIGN KEY REFERENCES Endereco(IdEndereco),
-	IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
-	NomeFantasia VARCHAR(70) NOT NULL,
-	CNPJ VARCHAR(14) NOT NULL,
-	Imagem VARCHAR NOT NULL
 );
 GO
 
 CREATE TABLE Cliente(
     IdCliente INT PRIMARY KEY IDENTITY(1,1),
-	IdEndereco INT FOREIGN KEY REFERENCES Endereco(IdEndereco),
 	IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
 	NomeCliente  VARCHAR(70) NOT NULL,
-	CPF VARCHAR(11) NOT NULL, 
-);
-GO
-
-CREATE TABLE ClienteBaixaRenda(
-    IdClienteBaixaRenda INT PRIMARY KEY IDENTITY(1,1),
-	IdEndereco INT FOREIGN KEY REFERENCES Endereco(IdEndereco),
-	IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
-	NomeClienteBaixaRenda VARCHAR(70) NOT NULL,
-	CPF VARCHAR(11) NOT NULL, 
-	NIS VARCHAR(11) NOT NULL,
+	CPF VARCHAR(11) NOT NULL UNIQUE, 
+	NIS VARCHAR(11) UNIQUE
 );
 GO
 
@@ -72,11 +51,19 @@ CREATE TABLE TipoProduto(
 );
 GO
 
+CREATE TABLE Finalidade(
+	IdFinalidade INT PRIMARY KEY IDENTITY(1,1),
+	Finalidade VARCHAR(20) NOT NULL UNIQUE
+);
+GO
+
 CREATE TABLE Produto(
 	IdProduto INT PRIMARY KEY IDENTITY(1,1),
+	IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
 	IdTipoProduto INT FOREIGN KEY REFERENCES TipoProduto(IdTipoProduto),
-	Quantidade INT NOT NULL,
-	Descricao VARCHAR(200) NOT NULL UNIQUE,
+	IdFinalidade INT FOREIGN KEY REFERENCES Finalidade(IdFinalidade),
+	Descricao VARCHAR(300) NOT NULL,
+	Quantidade SMALLINT NOT NULL,
 	Preco FLOAT NOT NULL,
 	DataValidade VARCHAR NOT NULL,
 	Imagem VARCHAR NOT NULL
@@ -93,15 +80,8 @@ CREATE TABLE Reserva(
 	IdReserva INT PRIMARY KEY IDENTITY(1,1),
 	IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
 	IdSituacaoReserva INT FOREIGN KEY REFERENCES SituacaoReserva(IdSituacaoReserva),
-	IdProduto INT FOREIGN KEY REFERENCES Produto(IdProduto),
-	DataReserva VARCHAR NOT NULL
+	IdProduto INT FOREIGN KEY REFERENCES Produto(IdProduto)
 );
 GO
 
-CREATE TABLE CarrinhoDeCompras(
-	IdCarrinhoDeCompras INT PRIMARY KEY IDENTITY(1,1),
-	IdProduto INT FOREIGN KEY REFERENCES Produto(IdProduto),
-	Quantidade INT NOT NULL
-);
-GO
 
