53905ae777b15c8b0f56e0aeb0d17314d289dc4a
livianegrini <liviasnegrini2004@gmail.com>
2022-03-03 02:14:34 -0300

    Alterando DML

--- BD/Scripts/Ofertas-DML.sql
+++ BD/Scripts/Ofertas-DML.sql
@@ -1,23 +1,35 @@
 USE OFERTA;
 GO
 
+SELECT * FROM TipoUsuario;
+SELECT * FROM Usuario;
+SELECT * FROM Endereco;
+SELECT * FROM Instituicao;
+SELECT * FROM Cliente;
+SELECT * FROM TipoProduto;
+SELECT * FROM Finalidade;
+SELECT * FROM Produto;
+SELECT * FROM SituacaoReserva;
+SELECT * FROM Reserva;
+GO
+
 INSERT INTO TipoUsuario (TipoUsuario) 
-VALUES ('Cliente comum'), ('Cliente baixa renda'), 
+VALUES ('Cliente Comum'), ('Cliente Baixa Renda'), 
 	   ('ONG'), ('Empresa');
 GO
 
 
 INSERT INTO Usuario (IdTipoUsuario, Email, Senha)
-VALUES (1,'Clientecomum@Gmail.com', 'comum123' ), (2,'Clientebaixa@Gmail.com', 'baixarenda123'), 
+VALUES (1,'ClienteComum@Gmail.com', 'omum123' ), (2,'ClienteBaixa@Gmail.com', 'baixarenda123'), 
 	  (3,'ONG@Gmail.com', 'ONG123'), (4,'Empresa@Gmail.com', 'empresa123');
 GO
 
 
 INSERT INTO Endereco (IdUsuario, Logadouro, Numero, Bairro, Municipio, Estado, CEP)
-VALUES (1,'Santa Catarina', '1463', 'São Cristóvão', 'Cuiabá', 'MT', '97504546'),
-	   (2,'Um', '99', 'Santo Antônio', 'Manaus', 'AM', '89858366'),
-	   (3,'Pará', '9844', 'Planalto', 'Porto Velho', 'RO', '79083236'),
-	   (4,'Mato Grosso', '342', 'Industrial', 'Boa Vista', 'RR', '69323894');
+VALUES (1,'Quadra Seis', '1463', 'Tabajara Brites', 'Uruguaiana', 'RS', '97504546'),
+	   (2,'Rua Lopes de Oliveira', '99', 'Barra Funda', 'São Paulo', 'SP', '01152010'),
+	   (3,'Rua Espírito Santo', '651', 'Cerâmica', 'São Caetano do Sul', 'SP', '09530701'),
+	   (4,'Rua George Eastman', '671', 'Vila Tramontano', 'São Paulo', 'SP', '05690000');
 GO
 
 
@@ -39,27 +51,27 @@ GO
 
 
 INSERT INTO Finalidade (Finalidade)
-VALUES ('Doação'), ('Compra'), ('Ofertado');
+VALUES ('Doação'), ('Compra'), ('Oferta');
 GO
 
 
 INSERT INTO Produto (IdUsuario, IdTipoProduto, IdFinalidade, Descricao, Quantidade, Preco, DataValidade, Imagem)
 VALUES (1, 2, 2, 'O Biscoito Recheado Trakinas possui uma deliciosa massa crocante e recheio saboroso. Sua embalagem é compacta, podendo ser levada para onde quiser. Ideal para qualquer ocasião', 
-		3, 2.00, 2024-07-03, 0000 ),
+		3, 2, '07/03/2024', '0450' ),
 		(2, 2, 1, 'TORCIDA® é a marca brasileira que conquistou todas as faixas etárias, inclusive os adultos, sendo o aperitivo perfeito para acompanhar os jogos de futebol e a roda de amigos.', 
-		1, 1.00, 2022-03-24, 0000 ),
+		1, 1, '03/02/2022', '5000' ),
 		(4, 2, 3, 'O prazer do sabor equilibrado, forte, com aroma intenso e que promove a cada amanhecer, experiências apaixonantes ao lado de quem mais amamos. Um clássico trazendo de suas raízes o gosto perfeito a cada xícara', 
-		1, 6.00, 2023-07-14, 0000 );
+		1, 6.5, '14/07/2023', '0560' );
 GO
 
 
 INSERT INTO SituacaoReserva (SituacaoReserva)
-VALUES ('Reservado'), ('Ainda não reservado');
+VALUES ('Reservado'), ('Não Reservado');
 GO
 
 INSERT INTO Reserva (IdUsuario, IdSituacaoReserva, IdProduto, QuantidadeReservada)
-VALUES (2, 1, 3, '2'),
-	   (1, 2, 1, '1');
+VALUES (2, 1, 3, 2),
+	   (1, 2, 1, 1);
 GO
 
 
