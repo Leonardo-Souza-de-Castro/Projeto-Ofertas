8a780a62256f9680eee943b84d1d8628d0b3ac5e
Lívia De Souza Negrini <46755453862@info.scs>
2022-03-03 10:20:23 -0300

    Startup configurada, Login iniciado, CRUD Usuario Iniciado

---
+++ Back End/senai_harmony_webAPI/senai_harmony_webAPI/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_harmony_webAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        //private IUsuarioRepository _UsuarioRepository { get; set; }
+    }
+}
---
+++ Back End/senai_harmony_webAPI/senai_harmony_webAPI/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using senai_harmony_webAPI.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_harmony_webAPI.Interfaces
+{
+    interface IUsuarioRepository
+    {
+
+        // <summary>
+        /// Valida o usuário
+        /// </summary>
+        /// <param name="Email">E-mail do usuário</param>
+        /// <param name="Senha">Senha do usuário</param>
+        /// <returns>Um objeto do tipo Usuario que foi encontrado</returns>
+        Usuario Login(string Email, string Senha);
+
+
+        //IFormFile Representa um arquivo enviado com o HttpRequest.
+        void SalvarPerfilBD(IFormFile foto, int id_usuario);
+        void SalvarPerfilDir(IFormFile foto, int id_usuario);
+        string ConsultarPerfilBD(int id_usuario);
+        string ConsultarPerfilDir(int id_usuario);
+
+
+        /// <summary>
+        /// Lista todos os usuários
+        /// </summary>
+        /// <returns>Uma lista de usuários</returns>
+        List<Usuario> Listar();
+    }
+}
